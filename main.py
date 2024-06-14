e3b74d374de81819f7db7d644954ea582604e98d
uhosamurai <166772425+uhosamurai@users.noreply.github.com>
2024-06-14 12:26:40 +0900

    uho

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "uhouho "}    
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
