445a689c31b8ebfa7fcb1a60d533c3dc4d2625f4
uhosamurai <166772425+uhosamurai@users.noreply.github.com>
2024-06-21 00:11:41 +0900

    変更

--- main.py
+++ main.py
@@ -47,4 +47,4 @@ def index():
 
     @app.post("/present")
 async def new_naming(present):
-    return {"response": f"サーバです。メリークリスマス！ {present}ありがとう。お返しはキャンディーです。"}
+    return {"response": f"うほー！！ {present}ありがとう。お返しはバナナうほー。"}
